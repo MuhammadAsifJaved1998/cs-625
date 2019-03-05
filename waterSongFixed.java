3c8725f19844a4fa0b148177b9ed243154a16272
MuhammadAsifJaved1998 <48000257+MuhammadAsifJaved1998@users.noreply.github.com>
2019-03-05 15:02:14 +0500

    First commit

--- waterSongFixed.java
+++ waterSongFixed.java
@@ -1,17 +1,18 @@
- class waterSong{
+ class waterSongFixed{
     public static void main(String[] args) {     
        int waterNum=9;
 String word="bottles";
 while(waterNum>0){
-    if(waterNum==1)
-	{
-	  word="bottle";
-	}
+ 
 	System.out.println(waterNum + " " + word + " of water on the wall");
 	System.out.println(waterNum + " " + word + " of water");
 	System.out.println("Take one down.");
 	System.out.println("Pass it around");
 	waterNum=waterNum-1;
+  if(waterNum==1)
+	{
+	  word="bottle";
+	}
            if(waterNum>0){
 	  System.out.println(waterNum + " " + word +" of water on the wall");
 	}
